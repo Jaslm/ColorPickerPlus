882c6bcf341d73523b72f63366b56d7add0ae9eb
Jaslm <70230626+Jaslm@users.noreply.github.com>
2021-05-19 10:11:30 -0700

    Add support for Burning Crusade Classic
    TBC uses same API changes as in original classic but has a different version number so this simply extends the isClassic test to include both.

--- ColorPickerPlus.lua
+++ ColorPickerPlus.lua
@@ -42,7 +42,8 @@ local gradientHeight = 160
 
 local colorSwatchWidth = 120
 local colorSwatchHeight = 120
-local isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC) -- true if on classic server
+-- local isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC) -- true if on classic server
+local isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC) or (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
 
 -- bgtable used in creation of backdrops
 local bgtable = {
